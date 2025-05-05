91b55c8c2de8546198695fb98d86b2a9ef44d15b
Tienn Kapilasena <tiennkapilasena@Tienns-Air.Home>
2025-05-05 17:01:49 +0100

    Fix API URL formatting

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const BASE_URL = window.location.hostname === "localhost"
     ? "http://127.0.0.1:5000"
-    : "https://igcse-toolkit.onrender.com;"
+    : "https://igcse-toolkit.onrender.com"
 
   const tabs = {
     home: document.getElementById("tab-home"),
